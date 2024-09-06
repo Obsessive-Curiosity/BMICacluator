8dbc46d54e927e260d1ea3c13e10d51c69a460d9
MIN JI-YEONG <kuck523@gmail.com>
2024-09-07 00:39:59 +0900

    Update useResult.jsx

--- src/components/Main/Hook/useResult.jsx
+++ src/components/Main/Hook/useResult.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 
 export default function useResult({ bmi }) {
   const [result, setResult] = useState("결과");
